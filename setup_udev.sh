36c92847d4825fc6255465f433de783fe1f53bb4
Stefan M <35365033+Darkmelman@users.noreply.github.com>
2018-04-27 18:38:30 +0200

    Update setup_udev.sh
    Correa Errors and add idVendor and idProduct to the UDEV file

--- setup_udev.sh
+++ setup_udev.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 # Create UDEV rule for switch in RCM Mode
 # create file fusee.sh
+
 if [ ! -e /home/pi/fusee.sh ]; then
-    echo "Creating fusee.sh"
-    cat <<EOF > /etc/udev/rules.d/100-switch.rules
-    #!/bin/bash
-    sleep 3
-    sudo python3 /home/pi/fusee-launcher.py fusee.bin
-    EOF
+echo "Creating fusee.sh"
+cat <<EOF > /home/pi/fusee.sh
+#!/bin/bash
+sleep 3
+sudo python3 /home/pi/fusee-launcher.py fusee.bin
+EOF
+sudo chmod +x /home/pi/fusee.sh
 else
-    echo "file already exists!
+    echo "file already exists!"
 fi
+
 cat <<EOF > /etc/udev/rules.d/100-switch.rules
-####################################################################################
 # change idVendor and idProduct
-# lsusb gives for example ID 0424:ec00
-# the string befoe the : is the idVendor and after the : idProduct
-#
-ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0424", ATTR{idProduct}=="ec00"
-, RUN+="/home/pi/fusee.sh"
-#####################################################################################
+# 0955:7321 NVidia Corp.
+ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="0955", ATTR{idProduct}=="7321", RUN+="/home/pi/fusee.sh"
 EOF
 
 sudo service udev restart
+
+#tail -f /var/log/syslog
+
